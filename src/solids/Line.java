5fe0f0b2266919463d7bb36518a40aa8a244bc74
Ivan Osipchyk <ivan.osipchyk@uhk.cz>
2023-12-16 15:29:52 +0100

    added line for 3 dimensional space

--- src/solids/Line.java
+++ src/solids/Line.java
@@ -1,2 +1,16 @@
-package solids;public class Line {
+package solids;
+
+import transforms.Point3D;
+
+import java.awt.*;
+import java.util.Arrays;
+
+public class Line extends Solid {
+    public Line(Color color, Point3D... points3D) {
+        this.vb.addAll(Arrays.asList(points3D));
+
+        addIndices(0, 1);
+
+        this.color = color;
+    }
 }
