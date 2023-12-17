ecf95156f542f9fb4d491ced01346c64048bc23f
Ivan Osipchyk <ivan.osipchyk@uhk.cz>
2023-12-17 20:27:54 +0100

    added comment

--- src/solids/Line.java
+++ src/solids/Line.java
@@ -5,6 +5,7 @@ import transforms.Point3D;
 import java.awt.*;
 import java.util.Arrays;
 
+// 3D Line between 2 points
 public class Line extends Solid {
     public Line(Color color, Point3D... points3D) {
         this.vb.addAll(Arrays.asList(points3D));
